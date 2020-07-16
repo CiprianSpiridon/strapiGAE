e58ce1be3371dae7744602a71c4023269d0d866e
Ciprian Spiridon <contact@ciprianspiridon.com>
2020-07-16 16:36:18 +0400

    updated DB to mongoDB

--- config/database.js
+++ config/database.js
@@ -2,13 +2,18 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', 'db/data.db'),
+        host: env('DATABASE_HOST', 'cluster0-smjup.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapiGAE2'),
+        username: env('DATABASE_USERNAME', 'ciprian'),
+        password: env('DATABASE_PASSWORD', '8JE0v88VuIgBBkyF'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '527345e8-def3-47dd-935d-75e9791c7c6c'
-};
